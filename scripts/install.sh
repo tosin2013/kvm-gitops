dfaa35c0984926a1cf4b53f311fb044d28003571
Tosin Akinosho <takinosh@redhat.com>
2022-08-24 21:10:36 -0400

    updating repo

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 set -xe 
-CHECKLOGGINGUSER=$(whoami)
-if [ ${CHECKLOGGINGUSER} == "root" ];
-then 
-  SUDO_USER=sudo
-fi
 
-
-${SUDO_USER} subscription-manager register
-${SUDO_USER}  subscription-manager refresh
-${SUDO_USER}  subscription-manager attach --auto
-${SUDO_USER}  subscription-manager repos --enable=rhel-8-for-x86_64-appstream-rpms --enable=rhel-8-for-x86_64-baseos-rpms
-${SUDO_USER}  dnf install git vim unzip wget bind-utils tar podman ansible -y 
+subscription-manager register
+subscription-manager refresh
+subscription-manager attach --auto
+subscription-manager repos --enable=rhel-8-for-x86_64-appstream-rpms --enable=rhel-8-for-x86_64-baseos-rpms
+dnf install git vim unzip wget bind-utils tar podman ansible jq python3-pip genisoimage nmstate -y 
 
 
 curl -OL https://raw.githubusercontent.com/tosin2013/openshift-4-deployment-notes/master/pre-steps/configure-openshift-packages.sh
@@ -20,14 +14,15 @@ chmod +x configure-openshift-packages.sh
 ./configure-openshift-packages.sh -i
 rm configure-openshift-packages.sh
 
+pip3 install jmespath
+
+git clone https://github.com/tosin2013/openshift-4-deployment-notes.git
 git clone https://github.com/tosin2013/openshift-virtualization-gitops.git
 cd openshift-virtualization-gitops
 ansible-galaxy install --force -r roles/requirements.yml
 ansible-galaxy collection install --force -r collections/requirements.yml
 
-wget -O gitea https://dl.gitea.io/gitea/1.16.9/gitea-1.16.9-linux-amd64
-chmod +x gitea
-useradd svc-gitea
+sudo useradd svc-gitea
 export GITEA_PASSWORD=$(openssl rand -base64 12) 
 export GITEA_URL=$(hostname -I | awk '{print $1}')
 echo "GITEA URL: http://${GITEA_URL}:3000" |  tee ${HOME}/gitea-password.txt
@@ -35,8 +30,6 @@ echo "USERNAME: svc-gitea"| tee -a ${HOME}/gitea-password.txt
 echo "PASSWORD: ${GITEA_PASSWORD}"| tee -a ${HOME}/gitea-password.txt
 cat ${HOME}/gitea-password.txt
 ansible-playbook -i  inventories/production/hosts configure-gitea.yml --extra-vars "gitea_admin=svc-gitea gitea_password=${GITEA_PASSWORD} endpoint=${GITEA_URL}"
+echo "cat ${HOME}/gitea-password.txt"
 
-curl -XPOST -H "Content-Type: application/json"  -k -d '{"name":"test"}' -u svc-gitea:${GITEA_PASSWORD} http:// ${GITEA_URL}:3000/api/v1/users/svc-gitea/tokens
 
-
-#sudo rm -rf  /home/svc-gitea/gitea/*
