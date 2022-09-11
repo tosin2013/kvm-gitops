a16c33c2ed593edd19153a3b77e1c49859c211b9
Tosin Akinosho <takinosh@redhat.com>
2022-09-11 12:38:04 -0400

    updating example script

--- scripts/example_script.sh
+++ scripts/example_script.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-
+# example example_script.sh kcli-openshift4-baremetal-dsal "http://yourrepo:3000/tosin/openshift-virtualization-gitops.git"
 if [ -z "$1" ]; then
     echo "No argument supplied"
     exit 1
 fi
 
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <directory_path> <git_url>"
+    exit 1
+fi
 
-GITURL="http://yourrepo:3000/tosin/openshift-virtualization-gitops.git"
+
+DIRECTORY_PATH=$1
+GITURL=$2
+
 if [ ! -d $HOME/openshift-virtualization-gitops ];
 then
     git clone $GITURL
 fi
 
-if [ ! -d $HOME/openshift-virtualization-gitops/inventories/${1} ];
+if [ ! -d $HOME/openshift-virtualization-gitops/inventories/${DIRECTORY_PATH} ];
 then
     echo "${1} Directory does not exist please validate it exists and try again"
     exit 1
@@ -30,7 +37,7 @@ targetConfigs:
   password: CHANGEME
   filetransfer:
   - name: copy-vars
-    targetPath: inventories/${1}/host_vars
+    targetPath: inventories/${DIRECTORY_PATH}/host_vars
     destinationDirectory: /home/admin/qubinode-installer/playbooks/vars
     schedule: "*/1 * * * *"
   branch: main
