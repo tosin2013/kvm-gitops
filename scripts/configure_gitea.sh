608ee8dc0baae4d463141d5c8112bbec9fcfcaad
Tosin Akinosho <takinosh@redhat.com>
2022-11-18 10:14:00 -0500

    adding gitea service

--- scripts/configure_gitea.sh
+++ scripts/configure_gitea.sh
@@ -7,7 +7,8 @@ sudo ansible-galaxy collection install --force -r collections/requirements.yml||
 
 if [ ${CONFIGURE_GITEA} == true ]; then
     sudo useradd svc-gitea
-    export GITEA_PASSWORD=$(openssl rand -base64 12) 
+    #export GITEA_PASSWORD=$(openssl rand -base64 12) 
+    export GITEA_PASSWORD=git3ap@33w0rd;
     export GITEA_URL=$(hostname -I | awk '{print $1}')
     echo "GITEA URL: http://${GITEA_URL}:3000" |  tee ${HOME}/gitea-password.txt
     echo "USERNAME: svc-gitea"| tee -a ${HOME}/gitea-password.txt
@@ -18,4 +19,7 @@ if [ ${CONFIGURE_GITEA} == true ]; then
     sudo  firewall-cmd --permanent --zone=public --add-port=3000/tcp
     sudo podman stop gitea
     sudo podman start gitea
+    sudo podman generate systemd --new gitea > gitea.service 
+    cp gitea.service /etc/systemd/system
+    systemctl daemon-reload
 fi
